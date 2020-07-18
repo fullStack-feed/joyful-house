9e90f9de7b2f9717814378aa2967bd380069c0e6
_上校 <53052047+sup-fiveyear@users.noreply.github.com>
2020-07-18 16:27:30 +0800

    数据库结构设计完毕

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,13 +1,15 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Database, Booking, Listing, User } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { useNewUrlParser: true });
   const db = client.db("main");
-
+  // collection 需要一个TSchema泛型，来约束从数据库拿回的数据类型
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,17 +1,92 @@
-import { Collection, ObjectId } from 'mongodb';
+import { Collection, ObjectId } from "mongodb";
+/**
+ * 约束房子类型，是别墅还是民宅...
+ *
+ * @export
+ * @enum {number}
+ */
+export enum ListingType {
+  Apartment = "apartment",
+  House = "house",
+}
 
+export interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+/**
+ * 对每个订单进行约束
+ *
+ * @export
+ * @interface Booking
+ */
+export interface Booking {
+  // 用于标识当前的订单号
+  _id: ObjectId;
+  // 用于标识当前对应的房子
+  lisitng: ObjectId;
+  // --- 以下字段用于标识：某用户的入住时间和退房信息
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
+  // ---
+}
+/**
+ * 对每个房子进行类型约束
+ *
+ * @export
+ * @interface Listing
+ */
 export interface Listing {
   _id: ObjectId;
   title: string;
+  description: string;
   image: string;
+  // PUZZ:房子拥有者？
+  host: string;
+  type: ListingType;
+  // --- 以下字段用于Google地图定位
   address: string;
+  country: string;
+  admin: string;
+  city: string;
+  // ---
+  bookings: ObjectId[];
+  // PUZZ:用于定义房子的日程？如果接客，设置为true？
+  bookingsIndex: BookingsIndexYear;
   price: number;
+  // 能够接客的人数
   numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
 }
-
+/**
+ * 对用户数据进行类型约束
+ *
+ * @export
+ * @interface User
+ *
+ * 以下两个字段是一对多关系:
+ *
+ * bookings： 一个用户可以有多个预定民宿
+ *
+ * listings： 一个用户可以拥有多个民宿进行出售
+ */
+export interface User {
+  _id: ObjectId;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  // 用户的所有收入
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
+}
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }
